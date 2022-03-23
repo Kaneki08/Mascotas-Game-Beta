5f63cf472d22cd1e18b78ebc3c66b83fa4eab9ab
Roberto Sanchez <robsanchez1772@gmail.com>
2022-03-22 19:15:36 -0700

    added videp

--- src/scenes/TitleScreen.js
+++ src/scenes/TitleScreen.js
@@ -2,7 +2,10 @@ import Phaser from "phaser";
 import start from "../assets/img/Play Button.png";
 import fullscreen from "../assets/img/whiteFullscreen.png";
 
+
 export default class TitleScreen extends Phaser.Scene {
+  video;
+
   constructor() {
     super("startGame");
   }
@@ -10,9 +13,19 @@ export default class TitleScreen extends Phaser.Scene {
   preload() {
     this.load.image("start", `${start}`);
     this.load.image("fullscreen", `${fullscreen}`);
+    this.load.video("start_video", `https://seals-ronjovi.s3.us-west-1.amazonaws.com/start_video.mp4`, false, true); // load start video
   }
 
   create() {
+    this.video = this.add.video(400, 300, "start_video"); // add video
+    this.video.setScale(1.2)
+    // Prevents video freeze when game is out of focus (i.e. user changes tab on the browser)
+    this.video.setPaused(false);
+    this.video.setLoop(true)
+    this.video.setMute(true);
+    this.video.play();
+
+
     const title = this.add.text(400, 250, "Mascotas");
     title.setOrigin(0.5, 9);
 
@@ -41,5 +54,9 @@ export default class TitleScreen extends Phaser.Scene {
       },
       this
     );
+  }
+
+  update() {
+
   }
 }
